c04e617fec47264425e07abfad26f50303cc5363
1834920 <106712896+1834920@users.noreply.github.com>
2022-08-03 19:47:56 -0400

    Working solution

--- helium.js
+++ helium.js
@@ -24,16 +24,34 @@ Feel free to use the status() method to output each player’s balloon count at 
 */
 
 class Player {
-    constructor(name, hitsPerMinute) {
-      this.name = name;
-      this.hitsPerMinute = hitsPerMinute;
-      this.balloonCount = 100;
-    }
-  
-    status() {
-      console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
-    }
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
   }
-  
-  // Write function below
-  
+
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
+  }
+}
+
+// Write function below
+const balloonAttack = (player1, player2) => {
+  //let hitBaloons = (this.hitsPerMinute * 10);
+  player1.balloonCount = player1.balloonCount - (player2.hitsPerMinute * 10);
+  player2.balloonCount = player2.balloonCount - (player1.hitsPerMinute * 10);
+if (player1.balloonCount > player2.balloonCount) {
+  return player1.name;
+} else if (player2.balloonCount > player1.balloonCount) {
+  return player2.name; 
+} else {
+  return 'Tie';
+}
+}
+
+const p1 = new Player('p1', 5);
+const p2 = new Player('p2', 2);
+ 
+console.log(balloonAttack(p1, p2));
+p1.status();
+p2.status();
