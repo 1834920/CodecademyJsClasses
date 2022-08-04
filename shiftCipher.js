0005b886a0919ab8043fc277b1d749627a0401a4
1834920 <106712896+1834920@users.noreply.github.com>
2022-08-03 20:13:41 -0400

    Create class; add links to charCode info

--- shiftCipher.js
+++ shiftCipher.js
@@ -16,3 +16,18 @@ Feel free to reference the Unicode Table as well as the JavaScript String method
 */
 
 // Write class below
+class ShiftCipher {
+    constructor(num) {
+      this.num = num;
+    }
+    encrypt() {
+  
+    }
+    decrypt() {
+  
+    }
+  }
+  
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+  
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
